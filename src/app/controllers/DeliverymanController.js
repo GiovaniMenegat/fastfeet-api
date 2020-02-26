28297d86fd20e0b4809c0a3f4d1a77d117f8748b
GiovaniMenegat <gmenegatsynapps@gmail.com>
2020-02-26 13:52:03 -0300

    feature: creates deliveryman and routes

---
+++ src/app/controllers/DeliverymanController.js
@@ -0,0 +1,86 @@
+import * as Yup from 'yup';
+import Deliveryman from '../models/Deliveryman';
+import File from '../models/File';
+
+class DeliverymanController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const deliverymanExists = await Deliveryman.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (deliverymanExists) {
+      return res.status(400).json({ error: 'Deliveryman already exists.' });
+    }
+
+    const { id, name, email } = await Deliveryman.create(req.body);
+    return res.json({
+      id,
+      name,
+      email,
+    });
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { email } = req.body;
+
+    const deliveryman = await Deliveryman.findByPk(req.deliveryId);
+
+    if (email && email !== deliveryman.email) {
+      const deliverymanExists = await Deliveryman.findOne({
+        where: { email },
+      });
+
+      if (deliverymanExists) {
+        return res.status(400).json({ error: 'Deliveryman already exists.' });
+      }
+    }
+
+    const { id, name } = await deliveryman.update(req.body);
+
+    return res.json({ id, name, email });
+  }
+
+  async index(req, res) {
+    const deliverymans = await Deliveryman.findAll({
+      attributes: ['id', 'name', 'email', 'avatar_id'],
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['name', 'path', 'url'],
+        },
+      ],
+    });
+
+    return res.json(deliverymans);
+  }
+
+  async delete(req, res) {
+    const deliverymans = await Deliveryman.findByPk(req.params.id);
+    deliverymans.destroy();
+
+    return res.json(deliverymans);
+  }
+}
+
+export default new DeliverymanController();
